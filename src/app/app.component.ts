1474eb97cd7aadac96de515a0a601a6b81850a5c
Shota Tamura <junkboy0315@gmail.com>
2017-10-17 11:37:24 +0900

    A rough migration to Angular 4 from Angular 2 was done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  totalPay: number = 0;
+  people: Person[] = [];
+  adjustmentUnit: number;
+
+  recalc() {
+    // 端数処理の単位を設定　5桁なら500円、4桁なら50円、3桁なら5円
+    let num = this.totalPay;
+    let keta = 0;
+    while(num >= 1) {
+      num /= 10;
+      keta += 1;
+    };
+    this.adjustmentUnit = 10 ** (keta - 3) * 5
+
+    // calculate the sum of payments marked 'Fix'
+    const countOfFixed = this.people
+      .filter(person => person.fixed === true)
+      .length;
+
+    const totalOfFixed = this.people
+      .filter(person => person.fixed === true)
+      .map(person => person.payment)
+      .reduce((prev, next) => { return prev + next }, 0);
+
+    // calculate payment for each person other than 'Fix'ed person
+    const paymentPerPerson = (this.totalPay - totalOfFixed) / (this.people.length - countOfFixed);
+    this.people
+    .filter(person => person.fixed === false)
+    .forEach((person) => {
+      person.payment = paymentPerPerson;
+    })
+
+  }
+
+  addPerson() {
+    this.people.push(new Person());
+    this.recalc();
+  }
+
+  deletePerson(index: number) {
+    this.people.splice(index, 1);
+    this.recalc();
+  }
+
+  increasePaymentOf(person: Person) {
+    person.payment -= person.payment % this.adjustmentUnit;
+    person.payment += this.adjustmentUnit;
+    person.fixed = true;
+
+    this.recalc();
+  }
+
+  decreasePaymentOf(person: Person) {
+    if(person.payment % this.adjustmentUnit != 0) {
+      person.payment -= person.payment % this.adjustmentUnit;
+    } else {
+      person.payment -= this.adjustmentUnit;
+    }
+    person.fixed = true;
+    this.recalc();
+  }
+
+  toggleFixedPayment(person: Person) {
+    person.fixed = !person.fixed;
+    this.recalc();
+  }
+}
+
+class Person {
+  payment: number;
+  fixed: boolean;
+
+  constructor() {
+    this.payment = 0;
+    this.fixed = false;
+  }
 }
