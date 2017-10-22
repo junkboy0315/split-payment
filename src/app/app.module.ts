49a746694edb95b0d79bce37e702a5d673da8544
Shota Tamura <junkboy0315@gmail.com>
2017-10-22 10:52:23 +0900

    add routing function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -9,6 +10,12 @@ import { HeaderComponent } from './header/header.component';
 import { PersonListService } from './shared/personlist.service';
 import { MainComponent } from './main/main.component';
 import { PersonComponent } from './main/person/person.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MainComponent },
+  // redirect all other path to the root('') path
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
@@ -21,6 +28,7 @@ import { PersonComponent } from './main/person/person.component';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [
     PersonListService,
