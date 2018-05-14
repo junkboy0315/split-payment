e2054939a1df5231004fd4e2d93cdd57fba1e836
stam <junkboy0315@gmail.com>
2018-05-15 08:25:03 +0900

    add sentry for error tracking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import * as Raven from 'raven-js';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -11,6 +12,16 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { PersonComponent } from './main/person/person.component';
 import { PersonListService } from './shared/personlist.service';
+
+Raven.config(
+  'https://5663add69d7645a39ea0e3acaa818a60@sentry.io/1206596',
+).install();
+
+export class RavenErrorHandler implements ErrorHandler {
+  handleError(err: any): void {
+    Raven.captureException(err);
+  }
+}
 
 const appRoutes: Routes = [
   { path: '', component: MainComponent },
@@ -35,7 +46,10 @@ const appRoutes: Routes = [
       enabled: environment.production,
     }),
   ],
-  providers: [PersonListService],
+  providers: [
+    { provide: ErrorHandler, useClass: RavenErrorHandler },
+    PersonListService,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
